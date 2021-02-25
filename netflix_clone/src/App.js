c714a3831003f36c69e66f08aaab1157c9a43313
Amy <amyjaderichards@googlemail.com>
2021-02-25 19:56:07 +0000

    Added Jumbotron, styles and GlobalStyles

--- netflix_clone/src/App.js
+++ netflix_clone/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
 import Jumbotron from './components/jumbotron';
+import jumbdoData from './fixtures/jumbo.json'
  
 export default function App() {
   return (
     <Jumbotron.Container>
-
+      {jumbdoData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
     </Jumbotron.Container>
   );
 }
+
 
