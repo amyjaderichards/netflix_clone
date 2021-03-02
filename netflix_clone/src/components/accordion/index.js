82838ed1c494092a1efe8dd12679e409b734030a
Amy <amyjaderichards@googlemail.com>
2021-03-02 08:30:51 +0000

    Adding FAQ styles and toggle

--- netflix_clone/src/components/accordion/index.js
+++ netflix_clone/src/components/accordion/index.js
@@ -29,10 +29,17 @@ Accordion.Item = function AccordionItem({children, ...restProps}) {
 }
 
 Accordion.Header = function AccordionHeader({children, ...restProps}) {
+    // eslint-disable-next-line
     const { toggleShow, setToggleShow } = useContext(ToggleContext);
     return (
         <Header onClick={() => setToggleShow((toggleShow) => !toggleShow)} {...restProps}>
             {children}
+            <pre>{JSON.stringify(toggleShow, null, 2)}</pre>
+            {toggleShow ? (
+                <img src="/images/icons/close-slim.png" alt="Close" />
+            ) : (
+                <img src="/images/icons/add.png" alt="Open" />
+            )}
         </Header>
     );
 };
