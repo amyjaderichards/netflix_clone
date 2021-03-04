cea0bbcf4c76059ab582522e9aad327e6e8395aa
Amy <amyjaderichards@googlemail.com>
2021-03-04 08:31:32 +0000

    WIP OptForm component

--- netflix_clone/src/containers/faqs.js
+++ netflix_clone/src/containers/faqs.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { Accordion } from '../components';
+import { Accordion, OptForm } from '../components';
 import faqsData from '../fixtures/faqs.json';
 
 export function FaqsContainer() {
@@ -14,6 +14,12 @@ export function FaqsContainer() {
                 </Accordion.Item>
             ))}
             <Accordion.Item />
+
+            <OptForm>
+                <OptForm.Input placeholder="Email Address" />
+                <OptForm.Button>Try it Now</OptForm.Button>
+                <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+            </OptForm>
         </Accordion>
     )
 }
